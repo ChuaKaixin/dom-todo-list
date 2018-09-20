486420b7abb38d922d80f7f87ad378f2212fd943
ChuaKaixin <chuakaixin.working@gmail.com>
2018-09-20 17:09:15 +0800

    first update

--- script.js
+++ script.js
@@ -4,3 +4,46 @@ const tasks = [
 	'nail javascript',
 	'give feedback',
 ];
+
+let tasklist = document.querySelector("#todo-list");
+
+const addToDo = (todoitem) => {
+	let listitem = document.createElement('li');
+	let textnode = document.createTextNode(todoitem);  
+	listitem.appendChild(textnode);
+	tasklist.appendChild(listitem);
+
+	listitem.addEventListener("click", function(event) {
+		listitem.classList.toggle('done')
+	  });
+}
+
+const handleToDoAdd = () => {
+	event.preventDefault(); 
+    let inputText = inputTextField.value
+    if (inputText == '') {
+        alert ('Please enter your to-do')
+	} 
+	else {
+		addToDo(inputText)
+		inputTextField.value = ''
+    }
+}
+
+for(var i=0; i < tasks.length; i++) {
+	addToDo(tasks[i])
+}
+
+let submitElement = document.querySelector("button");
+let inputTextField = document.querySelector("#inputField")
+
+submitElement.addEventListener("click", function(event) {
+	handleToDoAdd();
+});
+
+  inputTextField.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        handleToDoAdd();
+    }
+});
+
